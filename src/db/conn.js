5067554c0cac0ab540280e9bce185d8b1effa513
Mishra Vinaykumar <vinaymishra9925@gmail.com>
2026-01-11 18:03:05 +0530

    database connection file with 2 methods use of ifie and anther use of callback function using async await rap them with try catch bloc

---
+++ src/db/conn.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import express from "express";
+import { DB_NAME } from "../constant.js";
+
+const app = express();
+const connDB = async()=>{
+    try {
+        const connctionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connection !! DBHOST ${connctionInstance.connect}`);
+        
+    } catch (error) {
+        console.log("MongoDB Connction failes error",error);
+        process.exit(1);        
+    }
+}
+
+
+export default connDB;
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (err) => {
+//             console.log("connection Failes ", err);
+//             throw err
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listing on port ${process.env.PORT}`);
+
+//         }) 
+//     } catch (error) {
+//         console.log("error in DB : ", error);
+//         throw error
+//     }
+// })();
