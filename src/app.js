28aa7a71d2ebefa5232cdfa9d4dc1d6caa05d7fd
Mishra Vinaykumar <vinaymishra9925@gmail.com>
2026-01-13 22:19:24 +0530

    add user routes to app setup user router and mount it on /api/v1/users

--- src/app.js
+++ src/app.js
@@ -22,5 +22,13 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// routes
+
+import userRouter from "./routes/user.router.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 
 export {app}
