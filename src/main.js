a5c0bbc203ba1b9cfba2a2c5f67bb144e2ef48b9
Mishra Vinaykumar <vinaymishra9925@gmail.com>
2026-01-13 16:12:27 +0530

    initialize environment variables and start server on successful DB connection

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 import dotenv from "dotenv";
 import connDB from "./db/conn.js";
+import { app } from "./app.js"
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
-
-connDB();
-
-const profile = {
-    userName : "Mishra-Vinay",
-    age : 23,
-    printDetails : function(){
-        console.log(`My name is ${this.userName} , and age is ${this.age}`)
-    }
-}
-
-profile.printDetails();
+connDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`SERVER IS RUNNIG AT PORT : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection fails !!",err);
+})
 
 // require('dotenv').config({path:'./env'})
